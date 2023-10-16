f955a4c31ce05a8b91fa6981738a03a944ae4afe
wingfieldjeff <wingfield.jeffrey@gmail.com>
2023-10-16 14:44:05 -0700

    set to tpa

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ except KeyError:
 if __name__ == "__main__":
     logger.info(f"Token value: {SOME_SECRET}")
 
-    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
+    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Tampa&country=US')
     if r.status_code == 200:
         data = r.json()
         temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Berlin: {temperature}')
+        logger.info(f'Weather in Tampa: {temperature}')
