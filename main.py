5e06dd601785332b4e392326c9ff6ea92d0a0b15
wingfieldjeff <wingfield.jeffrey@gmail.com>
2023-10-16 15:13:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import logging
 import logging.handlers
 import os
-
+import dominate
+from dominate.tags import *
 import requests
+import datetime
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -16,19 +18,24 @@ formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(messag
 logger_file_handler.setFormatter(formatter)
 logger.addHandler(logger_file_handler)
 
-try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
-except KeyError:
-    SOME_SECRET = "Token not available!"
-    #logger.info("Token not available!")
-    #raise
+
 
 
 if __name__ == "__main__":
-    logger.info(f"Token value: {SOME_SECRET}")
+    
 
     r = requests.get('https://weather.talkpython.fm/api/weather/?city=Tampa&country=US')
     if r.status_code == 200:
         data = r.json()
         temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Tampa: {temperature}')
+        logger.info(f'Weather in Tampa: {temperature}C')
+        current_datetime = datetime.datetime.now()
+
+        d =dominate.document(title='Github Actions Demo')
+        d += h1('Weather in Tampa:')
+        d += h2(f'{temperature}C')
+        d += p(f'Last Updated: {current_datetime}')
+
+        html_content = d.render()
+        with open("index.html", "w") as file:
+            file.write(html_content)
